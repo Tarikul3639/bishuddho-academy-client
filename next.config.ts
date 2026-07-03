efcf621793b3cff9753b602d6ed3a322d10de348
Tarikul Islam <tarikul3639@gmail.com>
2026-07-04 04:45:43 +0600

    next.Config file update

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,17 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  reactCompiler: true,
+const BACKEND_URL =
+    process.env.BACKEND_URL ?? "http://localhost:5000";
 
-  images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "5000",
-      },
-    ],
-  },
+const nextConfig: NextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: "/api/:path*",
+                destination: `${BACKEND_URL}/api/:path*`,
+            },
+        ];
+    },
 };
 
 export default nextConfig;
