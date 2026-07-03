54bb78c8a62abd7f6149b92e05db881cabf2860b
Tarikul Islam <tarikul3639@gmail.com>
2026-07-04 04:51:54 +0600

    next.Config file update

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,13 @@
 import type { NextConfig } from "next";
 
-const BACKEND_URL =
-    process.env.BACKEND_URL ?? "http://localhost:5000";
+const BACKEND_URL = process.env.BACKEND_URL ?? "http://localhost:5000";
 
 const nextConfig: NextConfig = {
     async rewrites() {
         return [
             {
-                source: "/api/:path*",
-                destination: `${BACKEND_URL}/api/:path*`,
+                source: "/api/v1/:path*",
+                destination: `${BACKEND_URL}/api/v1/:path*`,
             },
         ];
     },
